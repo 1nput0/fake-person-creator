9b7910e3d39dd07be32ee9ab7cd4c8c31f96bf2a
1nput0 <beko365x@gmail.com>
2024-09-11 00:39:54 +0200

    add constructor function NewMalePerson()

--- faker/faker.go
+++ faker/faker.go
@@ -1,9 +1,23 @@
 package faker
 
+import "github.com/Pallinder/go-randomdata"
+
 // basic Person struct for generating fast fake data
 type Person struct {
 	firstName string
 	lastName  string
 	email     string
 	country   string
+}
+
+// constructor for creating a new male person with random fake data
+func NewMalePerson() Person {
+	newMale := Person{
+		firstName: randomdata.FirstName(randomdata.Male),
+		lastName:  randomdata.LastName(),
+		email:     randomdata.Email(),
+		country:   randomdata.Country(randomdata.FullCountry),
+	}
+
+	return newMale
 }
