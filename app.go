8123a03a04ce1a5995072c5ec4cbf90286ca7442
1nput0 <beko365x@gmail.com>
2024-09-12 22:53:14 +0200

    create new router and define api endpoints / create getMaleRandomPerson http handler

--- app.go
+++ app.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"net/http"
 
+	"github.com/1nput0/fake-person-creator/faker"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	// defining api endpoints
+	r.HandleFunc("/api/male-person", getMaleRandomPerson).Methods("GET")
+
+	// start the server
+	http.ListenAndServe(":8080", r)
+}
+
+func getMaleRandomPerson(w http.ResponseWriter, r *http.Request) {
+	// generate a random male person
+	newMale := faker.NewMalePerson()
+
+	// convert the person struct to json
+	jsonData, err := json.Marshal(newMale)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Set the content type and write the response
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
